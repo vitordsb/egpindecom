6b21de0acea5c65d79b35b1f5d79399fe77633a1
vitordsb <vitordsb2019@gmail.com>
2025-10-15 10:10:50 -0300

    Automatic Deploy

--- src/pages/manualPage.jsx
+++ src/pages/manualPage.jsx
@@ -4,12 +4,10 @@ import { FileText, Download } from "lucide-react";
 
 const ManualPage = () => {
   const manuals = [
-    { name: "DENKI 1.2", link: "/manuaisegp/denki12/Denki12.pdf" },
-    { name: "DENKI 5.0", link: "/manuaisegp/denk5/Denki5.pdf" },
-    { name: "EGP12V / EGP15V / EGP20V", link: "/manuaisegp/eco12v/Manual_EGP_12v_15v_20v.pdf" },
-    { name: "MÓDULO WIFI PLUGIN", link: "/manuaisegp/egpplugin/Manual_Modulo_EGP_PLUGIN_WIFI.pdf" },
-    { name: "ECO 10.000 / EGP 12.000 / CHRONOS / 20.000", link: "/manuaisegp/eco12v/Manual_EGP_12v_15v_20v.pdf" },
-    { name: "EGP NOBREAK / 12V3A / 12V6A", link: "/manuaisegp/egpnobreak/EGP_Nobreak.pdf" },
+    { name: "EGP12V / EGP15V / EGP20V", link: "/manuaisegp/eco12v/ManualEGP12v15v20v.pdf" },
+    { name: "MÓDULO WIFI PLUGIN", link: "/manuaisegp/egpplugin/ManualModuloEGPPlugInWifi.pdf" },
+    { name: "ECO 10.000 / EGP 12.000 / CHRONOS / 20.000", link: "/manuaisegp/eco12v/ManualEGP12v15v20v.pdf" },
+    { name: "EGP NOBREAK / 12V3A / 12V6A", link: "/manuaisegp/egpnobreak/EGPNobreak.pdf" },
     { name: "EGP AUTOMATIZADOR 520/640/760", link: "/manuaisegp/centralmonofasicaEGP520/centralmonofasica.pdf" },
     { name: "TEKNOGAM", link: "/manuaisegp/teknogam/teknogam.pdf" },
     { name: "FONTE 12V 1,5A", link: "/manuaisegp/eco12v/Manual_EGP_12v_15v_20v.pdf" },
