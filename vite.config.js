8c24aafeec340ba68566be3b7dbbba7598cc242d
vitordsb <vitordsb2019@gmail.com>
2025-10-15 08:41:16 -0300

    Automatic Deploy

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,11 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: './',
 })
